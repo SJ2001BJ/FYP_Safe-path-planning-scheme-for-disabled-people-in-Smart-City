94796b9c17f58a9ed3e881d89e13c3623682725b
SJ2001BJ <1773780642@qq.com>
2023-01-24 01:45:45 +0000

    The visualization test of the grid graph is completed, and the Dijkstra algorithm is used. The next step is to go deep into the core part of the algorithm and simulate the travel habits of the disabled.

--- testingVisualization.py
+++ testingVisualization.py
@@ -1,5 +1,6 @@
 import pygame
 import sys #module system
+from tkinter import  messagebox, Tk
 
 #Step 1: Create a pygame window
 #Step 2: Create a grid
@@ -16,38 +17,61 @@ boxWidth = window_width//columns
 boxHeight = window_height//rows
 
 grid = []
-
+queue = []
+path = []
 class Box:
-    def __init__(self,i,j):
+    def __init__(self, i, j):
         self.x = i
         self.y = j
         self.start = False
         self.wall = False
         self.destination = False
+        self.queued = False
+        self.visited = False
+        self.neighbours = []
+        self.prior = None
     def draw(self,win,color):
         pygame.draw.rect(win,color,(self.x * boxWidth, self.y * boxHeight, boxWidth - 2, boxHeight - 2))
         #Add margin
 
+    def set_neighbours(self):
+        if self.x > 0:
+            self.neighbours.append(grid[self.x - 1][self.y])
+        if self.x < columns - 1:
+            self.neighbours.append(grid[self.x + 1][self.y])
+        if self.y > 0:
+            self.neighbours.append(grid[self.x][self.y - 1])
+        if self.y < rows - 1:
+            self.neighbours.append(grid[self.x][self.y + 1])
 
 
 #Create grid
 for i in range(columns):
     arr = []
     for j in range(rows):
-        arr.append(Box(i,j))
+        arr.append(Box(i, j))
     grid.append(arr)
+
+#Set Neighbours
+for i in range(columns):
+    for j in range(rows):
+        grid[i][j].set_neighbours()
+
+
 
 #The starting point is located at the top left vertex of the entire grid graph
 startBox = grid[0][0]
 startBox.start = True
+startBox.visited = True
+queue.append(startBox)
+
+
 
 def main():
     beginSearch= False
     destination_box_set = False
-
-
-
-
+    searching = True
+    destination_box = None
     while True:
         # Close window
         for event in pygame.event.get():
@@ -74,23 +98,47 @@ def main():
 
           # Algorithm implementation
           if event.type == pygame.KEYDOWN and destination_box_set:
-              begin_search = True
+              beginSearch = True
 
+        if beginSearch:
+            if len(queue) > 0 and searching:
+                current_box = queue.pop(0)
+                current_box.visited = True
+                if current_box == destination_box:
+                    searching = False
+                    while current_box.prior != startBox:
+                        path.append(current_box.prior)
+                        current_box = current_box.prior
+                else:
+                    for neighbour in current_box.neighbours:
+                        if not neighbour.queued and not neighbour.wall:
+                            neighbour.queued = True
+                            neighbour.prior = current_box
+                            queue.append(neighbour)
+            else:
+                if searching:
+                    Tk().wm_withdraw()
+                    messagebox.showinfo("No Solution", "There is no solution!")
+                    searching = False
 
-
-
-
-
-        window.fill((0,0,0))
+        window.fill((0, 0, 0))
 
         for i in range(columns):
             for j in range(rows):
                 box = grid[i][j]
-                box.draw(window,(20,20,20))
+                box.draw(window,(100,100,100))
+
+                if box.queued:
+                    box.draw(window, (200, 0, 0))
+                if box.visited:
+                    box.draw(window, (0, 200, 0))
+                if box in path:
+                    box.draw(window, (0, 0, 200))
+
                 if box.start:
-                     box.draw(window,(0, 200, 200))
+                     box.draw(window, (0, 200, 200))
                 if box.wall:
-                     box.draw(window, (90, 90, 90))
+                     box.draw(window, (10, 10, 10))
                 if box.destination:
                      box.draw(window, (200, 200, 0))
         pygame.display.flip()
